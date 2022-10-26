4568d0125a660587fafb9dcf9534f493098b2104
patriicke <patrickndayambaje4@gmail.com>
2022-10-26 14:54:31 +0200

    I have added the tailwindcss for styling

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [value, setValue] = useState();
-
     return (
         <div>
-            <h1>Hello World</h1>
-            <h1>Hello World</h1>
+            <h1 className="text-red-500">Hello World</h1>
+            <h1 className="text-green-500 italic">Hello World</h1>
             <h1>Hello World</h1>
         </div>
     );
