410365db94baaadfc3fb5bc9841ba32cf25bfeec
patriicke <patrickndayambaje4@gmail.com>
2022-10-29 11:08:29 +0200

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Hacker News</title>
-        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="shortcut icon" href="./../js/assets/">
+        <link rel="preconnect" href="https://fonts.googleapis.com/logo.png">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&display=swap" rel="stylesheet">
         @viteReactRefresh
